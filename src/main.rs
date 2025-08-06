f46d9ac800da141a2d50ae5756db0d7fff8c9bf1
Nito <100199931+Nitoryu008@users.noreply.github.com>
2025-08-06 11:35:32 +0900

    2D rendering test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::color::palettes::css::*;
+use bevy::color::palettes::tailwind::*;
 use bevy::prelude::*;
 
 fn main() {
@@ -18,5 +19,9 @@ fn setup(
     let mesh = Mesh2d(meshes.add(Circle::new(100.)));
     let material = MeshMaterial2d(materials.add(Color::from(SKY_BLUE)));
     let transform = Transform::from_xyz(0., 0., 0.);
+    commands.spawn((mesh, material, transform));
+    let mesh = Mesh2d(meshes.add(Rectangle::new(10., 50.)));
+    let material = MeshMaterial2d(materials.add(Color::from(RED_900)));
+    let transform = Transform::from_xyz(200., 10., 0.);
     commands.spawn((mesh, material, transform));
 }
